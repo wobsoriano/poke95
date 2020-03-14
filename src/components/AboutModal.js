48ab3f9c4e6f9ad212452d388c3183be06ee6202
Robert Anthonie Soriano <sorianorobertc@gmail.com>
2020-03-14 13:21:13 +0800

    [refactor] Add credits

--- src/components/AboutModal.js
+++ src/components/AboutModal.js
@@ -60,6 +60,24 @@ const AboutModal = () => {
           </Anchor>
           .
         </p>
+        <p className="mt2">
+          {'Icons '}{' '}
+          <Anchor
+            href="https://artage.io/en/icon-packs/original-windows-95-icons"
+            target="_blank"
+          >
+            downloaded here.
+          </Anchor>
+        </p>
+        <p className="mt2">
+          {'Startup sound '}{' '}
+          <Anchor
+            href="http://soundbible.com/1654-Windows-95-Startup.html"
+            target="_blank"
+          >
+            downloaded here.
+          </Anchor>
+        </p>
       </WindowContent>
     </Window>
   );
