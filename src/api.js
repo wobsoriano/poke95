2c5a0ca5158ecc7f61eee98a9acd2eebe1904c0d
wobsoriano <sorianorobertc@gmail.com>
2023-11-30 18:04:54 -0800

    remove redundant throws

--- src/api.js
+++ src/api.js
@@ -1,36 +1,28 @@
 import { $fetch } from 'ohmyfetch';
 
 const getPokemon = async pokemonId => {
-  try {
-    const [data, species] = await Promise.all([
-      $fetch(`https://pokeapi.co/api/v2/pokemon/${pokemonId}`),
-      $fetch(`https://pokeapi.co/api/v2/pokemon-species/${pokemonId}`),
-    ]);
+  const [data, species] = await Promise.all([
+    $fetch(`https://pokeapi.co/api/v2/pokemon/${pokemonId}`),
+    $fetch(`https://pokeapi.co/api/v2/pokemon-species/${pokemonId}`),
+  ]);
 
-    const flavorTextEntry = species.flavor_text_entries.findIndex(i => {
-      return i.language.name === 'en' && i.version.name === 'y';
-    });
-    data.flavor_text = species.flavor_text_entries[flavorTextEntry].flavor_text;
+  const flavorTextEntry = species.flavor_text_entries.findIndex(i => {
+    return i.language.name === 'en' && i.version.name === 'y';
+  });
+  data.flavor_text = species.flavor_text_entries[flavorTextEntry].flavor_text;
 
-    return data;
-  } catch (e) {
-    throw e;
-  }
+  return data;
 };
 
 const getAllPokemons = async () => {
-  try {
-    const data = await $fetch('https://pokeapi.co/api/v2/pokemon?limit=151');
-    return data.results.map((i, idx) => {
-      return {
-        id: idx + 1,
-        name: i.name,
-        selected: false,
-      };
-    });
-  } catch (e) {
-    throw e;
-  }
+  const data = await $fetch('https://pokeapi.co/api/v2/pokemon?limit=151');
+  return data.results.map((i, idx) => {
+    return {
+      id: idx + 1,
+      name: i.name,
+      selected: false,
+    };
+  });
 };
 
 export { getPokemon, getAllPokemons };
